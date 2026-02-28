aad4d6481df2a89ba685dce5fc8f66d4c006c109
Stanislav Rodionov <steve@knyte.space>
2026-02-28 07:08:03 +0300

    Fix indents by replacing tabs with spaces

--- preload_space.js
+++ preload_space.js
@@ -1,15 +1,15 @@
 let space_number = 0
 const argt = `--window-caption-number=`
 for (let i = 0; i < process.argv.length; ++i) {
-	const arg = process.argv[i]
-	if (arg.indexOf(argt) < 0)
-		continue
-	space_number = parseInt(arg.split(argt)[1])
-	break
+  const arg = process.argv[i]
+  if (arg.indexOf(argt) < 0)
+    continue
+  space_number = parseInt(arg.split(argt)[1])
+  break
 }
 window.addEventListener('DOMContentLoaded', () => {
-	document.title = `${document.title} ${space_number}`
-	const e = document.getElementById('placeholder')
-	e.textContent = `${e.textContent} ${space_number}`
+  document.title = `${document.title} ${space_number}`
+  const e = document.getElementById('placeholder')
+  e.textContent = `${e.textContent} ${space_number}`
 })
 console.log(`preload_space.js ${space_number} ready`)
