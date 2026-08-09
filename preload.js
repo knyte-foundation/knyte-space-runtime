194fe968d0c813ae88d32329d9e9ccd3373dda52
Stanislav Rodionov <steve@knyte.space>
2026-08-09 11:40:34 +0300

    test version control

--- preload.js
+++ preload.js
@@ -25,7 +25,7 @@ window.addEventListener('DOMContentLoaded', () => {
 			document.getElementById('app-instance-id').textContent = app_instance_id
 			document.getElementById('root-path').textContent = app_root_path
 			document.getElementById('db-path').textContent = db_path
-			console.log(app_version)
+			console.log('app_version', app_version)
 		})
 	document.getElementById('button-memtest-node').addEventListener('click', () => {
 		ipcRenderer
